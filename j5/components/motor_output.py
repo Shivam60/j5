5346b5507f93dbfa718aff02630df1de5e16ac9a
Anton Nikitin <antonnikitin@live.co.uk>
2019-02-04 19:43:44 +0000

    change code to use properties + getter/setter

--- j5/components/motor_output.py
+++ j5/components/motor_output.py
@@ -8,6 +8,11 @@ class MotorOutputInterface(metaclass=ABCMeta):
     """An interface containing the methods to control a motor output"""
 
     @abstractmethod
+    def get_motor_power(self, board: Board, identifier: int):
+        """Gets the output power of a motor"""
+        raise NotImplementedError
+
+    @abstractmethod
     def set_motor_power(self, board: Board, identifier: int, power: int):
         """Set the output power"""
         raise NotImplementedError
@@ -27,6 +32,15 @@ class MotorOutput(Component):
         return MotorOutputInterface
 
     @property
-    def set_motor_power(self, power_value: int):
+    def motor_power(self) -> int:
+        """Get the power value of the motor"""
+        return self._backend.get_motor_power(self._board, self._identifier)
+
+    @motor_power.setter
+    def motor_power(self, power_value: int):
         """Set the power value for a motor output"""
+        if abs(power_value) > 100:
+            """Checks if value is within range"""
+            raise ValueError("Motor speed value out of range! Should be between -100 and 100!")
+        
         self._backend.set_motor_power(self._board, self._identifier, power_value)
