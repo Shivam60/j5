f0f84c98f5117958fe2a477951bb0fcacba1798a
Anton Nikitin <antonnikitin@live.co.uk>
2019-02-04 19:23:05 +0000

    add new MotorOutput component

---
+++ j5/components/motor_output.py
@@ -0,0 +1,32 @@
+from abc import ABCMeta, abstractmethod
+
+from j5.boards import Board
+from j5.components import Component
+
+
+class MotorOutputInterface(metaclass=ABCMeta):
+    """An interface containing the methods to control a motor output"""
+
+    @abstractmethod
+    def set_motor_power(self, board: Board, identifier: int, power: int):
+        """Set the output power"""
+        raise NotImplementedError
+
+
+class MotorOutput(Component):
+    """An output port on the motor board"""
+
+    def __int__(self, identifier: int, board: Board, backend: MotorOutputInterface):
+        self._board = board
+        self._backend = backend
+        self._identifier = identifier
+
+    @staticmethod
+    def interface_class():
+        """Get the interface class that is required to use this component"""
+        return MotorOutputInterface
+
+    @property
+    def set_motor_power(self, power_value: int):
+        """Set the power value for a motor output"""
+        self._backend.set_motor_power(self._board, self._identifier, power_value)
